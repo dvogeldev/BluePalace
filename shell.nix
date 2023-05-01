bd8cf83ed22d9e38a8e4abae2d67983570d37e8f
David Vogel <david@dvogeldev.com>
2023-05-01 11:39:46 -0400

    Added shell configs

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs, config, ... }:
+{
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    bashrcExtra = ''
+      set - o vi
+    '';
+    profileExtra = "[ -r ~/.nix-profile/etc/profile.d/nix.sh ] && source ~/.nix-profile/etc/profile.d/nix.sh";
+  };
+  programs.zoxide = {
+    enableBashIntegration = true;
+  };
+  programs.exa = {
+    enableAliases = true;
+    extraOptions = [
+      "--group-directories-first"
+      "--header"
+    ];
