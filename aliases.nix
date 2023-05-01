ba4af01ad7eb971b5e5c0dad3e3f7b1cd99b257d
David Vogel <david@dvogeldev.com>
2023-05-01 11:16:01 -0400

    Add aliases

--- aliases.nix
+++ aliases.nix
@@ -1,11 +1,17 @@
 { pkgs, misc, ... }: {
    home.shellAliases = {
-    apply-dvpc = "nix run --impure home-manager/master -- -b bak switch --flake .#bjk@beast";
+    apply-dvpc = "nix run --impure home-manager/master -- -b bak switch --flake .#david";
     
     # bat --plain for unformatted cat
     catp = "bat -P";
     
     # replace cat with bat
     cat = "bat";
+
+    # utilites
+    cl = "clear";
+    cls = "clear && exa -lh";
+    cla = "clear && exa -lah";
+
     };
 }
