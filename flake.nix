8d6813ad269bd307ce47f33d9ed2b3287294beef
David Vogel <david@dvogeldev.com>
2023-05-01 12:37:47 -0400

    Add nix-colors

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-colors.url = "github:misterio77/nix-colors";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: {
