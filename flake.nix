68fc90635cdaf2e633df73df2fc676ab23e64ebf
David Vogel <david@dvogeldev.com>
2023-05-02 13:48:19 -0400

    Add stylix

--- flake.nix
+++ flake.nix
@@ -6,25 +6,22 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url = "github:danth/stylix";
-    stylix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, stylix, ... }: {
 
-    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-    #defaultPackage = stylix.packages.x86_64-linux.stylix-nix;
+    #defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
 
     homeConfigurations = {
       "david" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
-	      extraSpecialArgs = { inherit nix-colors; };
 
 	      modules = [
 	        ./home.nix
 	        ./aliases.nix
 	        ./programs.nix
 	        ./shell.nix
-	        stylix.homeManagerModules.stylix
+	        #stylix.homeManagerModules.stylix
 	      ];
       };
     };
