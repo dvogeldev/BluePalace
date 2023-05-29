8fd7229ebca2f79b13fcc167ef11c78ffe37d74b
david <david@Edge.dvpc>
2023-05-29 18:29:23 -0400

    Updated flake

--- gnome-shortcuts.nix
+++ gnome-shortcuts.nix
@@ -4,12 +4,12 @@ let
   keysBackup = pkgs.writeShellScriptBin "keysBackup" ''
     dconf dump / | sed -n '/\[org.gnome.settings-daemon.plugins.media-keys/,/^$/p' > $HOME/.config/home-manager/data/custom-shortcuts.conf # Export
   '';
-  keysImport = pkgs.writeShellScriptBin "keysImport" ''
-    dconf load / < $HOME/.config/home-manager/data/custom-shortcuts.conf # Import
-  '';
+#  keysImport = pkgs.writeShellScriptBin "keysImport" ''
+#    dconf load / < $HOME/.config/home-manager/data/custom-shortcuts.conf # Import
+#  '';
 
 
 in {
-  environment.systemPackages = [ keysBackup keysImport ];
+  environment.systemPackages = [ keysBackup ];
 }
 
